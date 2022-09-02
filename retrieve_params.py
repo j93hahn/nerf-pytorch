e63c172066b88c18c816307866dffb57cafe97d3
Joshua Ahn <jjahn@archimedes.ttic.edu>
2022-09-02 14:29:19 -0500

    refactored

--- retrieve_params.py
+++ retrieve_params.py
@@ -1,53 +1,54 @@
 import matplotlib.pyplot as plt
-import argparse
+import configargparse
 import torch
 import numpy as np
 
 
-plt.style.use('seaborn-whitegrid')
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
+def config_parser():
+    parser = configargparse.ArgumentParser(
         description='Load pretrained NeRF model')
     parser.add_argument(
-        '-o',
+        '--object',
         default='lego',
         choices=['fern', 'flower', 'fortress', 'horns', 'lego', 'trex'],
         help='Specify the model you would you like to retrieve')
+    parser.add_argument(
+        '--model',
+        default='fn',
+        choices=['fn', 'fine'],
+        help='Specify the state dictionary to retrieve')
 
-    args = vars(parser.parse_args())
-
-    number = '2' if args['o'] in ['fern', 'lego'] else '1'
-    return 'nerf-pytorch/logs/' + args['o'] + '_test/' + number + '00000.tar'
+    return parser
 
 
-"""
-network_fn is used to sample 64 "fine" points, while network_fine samples those
-64 "fine" points plus 128 "coarse" points = 192 total points. in the case
-network_fine is None, then network_fn is used.
-"""
+# network_fn is used to sample 64 "fine" points, while network_fine samples those
+# 64 "fine" points plus 128 "coarse" points = 192 total points. in the case
+# network_fine is None, then network_fn is used.
+def process_args(args):
+    params = 'network_' + args.model + '_state_dict'
+    number = '2' if args.object in ['fern', 'lego'] else '1'
+    loc = 'nerf-pytorch/logs/' + args.object + '_test/' + number + '00000.tar'
+    return params, loc
+
+
 def main():
-    location = parse_args()
-    ckpt = torch.load(location)
-    network_fn = ckpt['network_fn_state_dict']
-    network_fine = ckpt['network_fine_state_dict']
+    parser = config_parser()
+    args = parser.parse_args()
+    params, loc = process_args(args)
+
+    ckpt = torch.load(loc)
+    network_weights = ckpt[params]
 
     #print("\nPrinting network_fn parameter groups\n")
-    for key in network_fn.keys():
+    for key in network_weights.keys():
         if key == 'alpha_linear.weight':
             #breakpoint()
-            print(network_fn[key].max())
-            x = np.histogram(network_fn[key].reshape(-1).cpu().numpy(), density=True)
+            print(network_weights[key].max())
+            x = np.histogram(network_weights[key].reshape(-1).cpu().numpy(), density=True)
             plt.plot(x[0])
             plt.savefig('first_view.png', dpi=300)
 
-    #print("\nPrinting network_fine parameter groups\n")
-    #for key in network_fine.keys():
-    #    print(key, network_fine[key].shape)
-
-    return
-
 
 if __name__ == '__main__':
+    plt.style.use('seaborn-whitegrid')
     main()
