80389e4e827dac3b738ce26f054e1c0bba9ef775
Joshua Ahn <jjahn@archimedes.ttic.edu>
2022-09-08 15:14:21 -0500

    Useful refactoring

--- nerf-pytorch/siren.py
+++ nerf-pytorch/siren.py
@@ -3,31 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torchsummary import summary
+from run_nerf_helpers import NeRF
 
 
-def sine_init(m):
-    with torch.no_grad():
-        if hasattr(m, 'weight'):
-            num_input = m.weight.size(-1)
-            # See supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
-
-
-def first_layer_sine_init(m):
-    with torch.no_grad():
-        if hasattr(m, 'weight'):
-            num_input = m.weight.size(-1)
-            # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-1 / num_input, 1 / num_input)
-
-
-class Sine(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input):
-        # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
-        return torch.sin(30 * input)
+# sine activation function - replaces ReLU in NeRF MLP, taken directly from SiREN
+def Sine(x):
+    return torch.sin(30 * x)
 
 
 # NeRF model implementation with MLP backbone replaced by SiREN
@@ -41,33 +22,11 @@ class SirenNeRF(nn.Module):
         self.skips = skips
         self.use_viewdirs = use_viewdirs
 
-
-    def forward(self, x):
-        ...
-
-
-# NeRF MLP model implementation from nerf-pytorch
-class NeRF(nn.Module):
-    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
-        """
-        """
-        super(NeRF, self).__init__()
-        self.D = D
-        self.W = W
-        self.input_ch = input_ch
-        self.input_ch_views = input_ch_views
-        self.skips = skips
-        self.use_viewdirs = use_viewdirs
-
+        # create layers here
         self.pts_linears = nn.ModuleList(
             [nn.Linear(input_ch, W)] + [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
 
-        ### Implementation according to the official code release (https://github.com/bmild/nerf/blob/master/run_nerf_helpers.py#L104-L105)
         self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W//2)])
-
-        ### Implementation according to the paper
-        # self.views_linears = nn.ModuleList(
-        #     [nn.Linear(input_ch_views + W, W//2)] + [nn.Linear(W//2, W//2) for i in range(D//2)])
 
         if use_viewdirs:
             self.feature_linear = nn.Linear(W, W)
@@ -76,12 +35,44 @@ class NeRF(nn.Module):
         else:
             self.output_linear = nn.Linear(W, output_ch)
 
+        self.init_weights()
+
+    # apply weight initialization here
+    def init_weights(self):
+        for i in range(len(self.pts_linears)):
+            init_func = SirenNeRF.first_layer_sine_init if i == 0 else SirenNeRF.sine_init
+            init_func(self.pts_linears[i])
+
+        SirenNeRF.sine_init(self.views_linears)
+
+        if self.use_viewdirs:
+            SirenNeRF.sine_init(self.feature_linear)
+            SirenNeRF.sine_init(self.alpha_linear)
+            SirenNeRF.sine_init(self.rgb_linear)
+        else:
+            SirenNeRF.sine_init(self.output_linear)
+
+    # the following two static methods were taken directly from SiREN
+    @staticmethod
+    def first_layer_sine_init(m):
+        with torch.no_grad():
+            if hasattr(m, 'weight'):
+                num_input = m.weight.size(-1)
+                m.weight.uniform_(-1 / num_input, 1 / num_input)
+
+    @staticmethod
+    def sine_init(m):
+        with torch.no_grad():
+            if hasattr(m, 'weight'):
+                num_input = m.weight.size(-1)
+                m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+
     def forward(self, x):
         input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
         h = input_pts
         for i, l in enumerate(self.pts_linears):
             h = self.pts_linears[i](h)
-            h = F.relu(h)
+            h = Sine(h) # change
             if i in self.skips:
                 h = torch.cat([input_pts, h], -1)
 
@@ -92,7 +83,7 @@ class NeRF(nn.Module):
 
             for i, l in enumerate(self.views_linears):
                 h = self.views_linears[i](h)
-                h = F.relu(h)
+                h = Sine(h) # change
 
             rgb = self.rgb_linear(h)
             outputs = torch.cat([rgb, alpha], -1)
@@ -102,9 +93,71 @@ class NeRF(nn.Module):
         return outputs
 
 
-def compare_models():
-    ...
+# modeled after create_nerf()
+def create_siren_nerf(args):
+    """Instantiate NeRF's SiREN MLP backbone model.
+    """
+    embed_fn, input_ch = get_embedder(args.multires, args.i_embed)
 
+    input_ch_views = 0
+    embeddirs_fn = None
+    if args.use_viewdirs:
+        embeddirs_fn, input_ch_views = get_embedder(args.multires_views, args.i_embed)
+    output_ch = 5 if args.N_importance > 0 else 4
+    skips = [4]
+    model = SirenNeRF(D=args.netdepth, W=args.netwidth,
+                      input_ch=input_ch, output_ch=output_ch, skips=skips,
+                      input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs).to(device)
+    grad_vars = list(model.parameters())
+
+    model_fine = None
+    if args.N_importance > 0:
+        model_fine = SirenNeRF(D=args.netdepth_fine, W=args.netwidth_fine,
+                               input_ch=input_ch, output_ch=output_ch, skips=skips,
+                               input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs).to(device)
+        grad_vars += list(model_fine.parameters())
+
+    network_query_fn = lambda inputs, viewdirs, network_fn : run_network(inputs, viewdirs, network_fn,
+                                                                embed_fn=embed_fn,
+                                                                embeddirs_fn=embeddirs_fn,
+                                                                netchunk=args.netchunk)
+
+    # Create optimizer
+    optimizer = torch.optim.Adam(params=grad_vars, lr=args.lrate, betas=(0.9, 0.999))
+
+    start = 0
+    basedir = args.basedir
+    expname = args.expname
+
+    render_kwargs_train = {
+        'network_query_fn' : network_query_fn,
+        'perturb' : args.perturb,
+        'N_importance' : args.N_importance,
+        'network_fine' : model_fine,
+        'N_samples' : args.N_samples,
+        'network_fn' : model,
+        'use_viewdirs' : args.use_viewdirs,
+        'white_bkgd' : args.white_bkgd,
+        'raw_noise_std' : args.raw_noise_std,
+    }
+
+    # NDC only good for LLFF-style forward facing data
+    if args.dataset_type != 'llff' or args.no_ndc:
+        print('Not ndc!')
+        render_kwargs_train['ndc'] = False
+        render_kwargs_train['lindisp'] = args.lindisp
+
+    render_kwargs_test = {k : render_kwargs_train[k] for k in render_kwargs_train}
+    render_kwargs_test['perturb'] = False
+    render_kwargs_test['raw_noise_std'] = 0.
+
+    return render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer
+
+
+def compare_models():
+    model1 = NeRF()
+    model2 = SirenNeRF()
+    breakpoint()
 
 if __name__ == '__main__':
     compare_models()
--- vis.py
+++ vis.py
@@ -6,15 +6,24 @@ plt.style.use('seaborn-whitegrid')
 
 
 def generate_scaled_alpha_plots():
-    x1, psnr1 = read_stats('experiments/no_scale_alpha/', 'psnr')
-    x2, psnr2 = read_stats('experiments/scale_alpha', 'psnr')
+    #x1, psnr1 = read_stats('experiments/no_scale_alpha/', 'psnr')
+    #x2, psnr2 = read_stats('experiments/scale_alpha', 'psnr')
+
+    x, psnrs = read_stats('nerf-pytorch/experiments/full_res_lego_siren_pos_emb/', 'psnr')
+
     fig, axs = plt.subplots()
-    plt.title("Experimenting with Different Scaling Factors on α for Vanilla NeRF MLP")
+    #plt.title("Experimenting with Different Scaling Factors on α for Vanilla NeRF MLP")
+
+    plt.title("Training PSNR Values for SiREN NeRF on Lego")
     plt.ylabel("PSNR Values")
     plt.xlabel("Training Iteration")
-    make_plot(axs, x1, psnr1, "No Scaling on α")
-    make_plot(axs, x2, psnr2, "α scaled by 30")
-    plt.savefig('combinedpsnr.png', dpi=300)
+    make_plot(axs, x, psnrs, "SiREN NeRF")
+
+    #make_plot(axs, x1, psnr1, "No Scaling on α")
+    #make_plot(axs, x2, psnr2, "α scaled by 30")
+
+
+    plt.savefig('siren_full_res.png', dpi=300)
 
 
 if __name__ == '__main__':
