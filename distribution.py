b470b34bd858620af956a19d8ec48994c97cd2a0
Joshua Ahn <jjahn@archimedes.ttic.edu>
2022-09-12 22:19:23 -0500

    automate generation script

--- distribution.py
+++ distribution.py
@@ -2,6 +2,7 @@ from fabric.utils.event import read_lined_json
 from utils import make_plot
 from typing import Union
 from pathlib import Path
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,8 +23,8 @@ def read_stats(dirname):
 # in each 12 points, stats[0:8] contain the standard 8-layer MLP. stats[8] contains
 # the alpha values, stats[9] has feature values, stats[10] has the "9th" layer,
 # stats[11] outputs the RGB values (before sigmoid activation)
-def retrieve(rendered_pose:int, layer:Union[int, str], stat:str):
-    stats = read_stats(dirname='../exp/nerf/22_0911_1206_52/pose_' + str(rendered_pose))
+def retrieve(pose:int, layer:Union[int, str], stat:str):
+    stats = read_stats(dirname='./')
     data = []
 
     def preprocessing(layer):
@@ -51,26 +52,38 @@ def retrieve(rendered_pose:int, layer:Union[int, str], stat:str):
     return np.array(data)
 
 
-def vis(data, rendered_pose):
+def vis(data, pose, element):
+    def element_to_title(element):
+        if element in range(8):
+            return f"Layer {element+1} Activation"
+        elif element == 'alpha':
+            return "Sigma"
+        elif element == 'rgb':
+            return "RGB"
+        elif element == 'feature':
+            return "'Feature'"
+        elif element == 'layer':
+            return "Layer 9 Activation"
+        else:
+            raise Exception("Invalid element input")
+
+    title = element_to_title(element)
+
     plt.hist(data[:, 0], bins=50, alpha=0.5, label="Means")
     plt.hist(data[:, 1], bins=50, alpha=0.5, label="Stds")
-    plt.title("'Features' Distribution for Pretrained Lego Rendered Pose " + str(rendered_pose))
+
+    plt.title(f"{title} Distribution for Pretrained Lego Rendered Pose " + str(pose+1))
     plt.xlabel("Values")
     plt.ylabel("Count")
     plt.legend()
-
-    os.chdir('../exp/nerf/22_0911_1206_52/pose_' + str(rendered_pose))
-
-    plt.savefig('features_distrib.png', dpi=300)
+    plt.savefig(f'{title} Distribution.png', dpi=300)
+    plt.clf()
 
 
 if __name__ == '__main__':
-    rendered_pose = 0
-    layer = 'feature'
-
-    means = retrieve(rendered_pose, layer, 'mean')
-    stds = retrieve(rendered_pose, layer, 'std')
-
-    data = np.stack([means, stds], axis=-1)
-
-    vis(data, rendered_pose)
+    pose = 0
+    for element in tqdm([0, 1, 2, 3, 4, 5, 6, 7, 'layer', 'feature', 'rgb', 'alpha']):
+        means = retrieve(pose, element, 'mean')
+        stds = retrieve(pose, element, 'std')
+        data = np.stack([means, stds], axis=-1)
+        vis(data, pose, element)
